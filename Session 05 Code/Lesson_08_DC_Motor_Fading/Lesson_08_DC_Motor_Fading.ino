c73345e2241c418820cbe2ccb3189d476a8b6652
Jared Lundholm <jlundholm@ucsd1.local>
2018-03-27 10:25:41 -0600

    added lessons 7 and 8 dc motors

---
+++ Session 05 Code/Lesson_08_DC_Motor_Fading/Lesson_08_DC_Motor_Fading.ino
@@ -0,0 +1,21 @@
+int motorPin = 3;
+
+void setup() {
+  // nothing happens in setup
+}
+
+void loop() {
+  // fade in from min to max in increments of 5 points:
+  for (int fadeValue = 0 ; fadeValue <= 255; fadeValue += 5) {
+    // sets the value (range from 0 to 255):
+    analogWrite(motorPin, fadeValue);
+    delay(30);
+  }
+
+  // fade out from max to min in increments of 5 points:
+  for (int fadeValue = 255 ; fadeValue >= 0; fadeValue -= 5) {
+    // sets the value (range from 0 to 255):
+    analogWrite(motorPin, fadeValue);
+    delay(30);
+  }
+}
